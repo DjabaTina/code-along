cb87b1b1544c540c5e7df770d213b2513eed407b
DjabaTina <djabaaugustina1@gmail.com>
2022-08-22 16:14:34 +0100

    Custom-hook

--- src/App.js
+++ src/App.js
@@ -6,21 +6,22 @@
 import ProductList from "./components/ProductList";
 // import TaskManager from "./components/TaskManager";
 // import Form from "./components/Form";
+import Spinner from "./components/Spinner";
 
 function App() {
   return (
     <div>
+      <ProductList />
       {/* <Navbar />
       <Header />
       <Button lable="Add to Cart" />
       <Button lable="Read mord..." />
       <Button lable="Register" />
       <Button lable="Login" />
-  <Image /> */}
+     <Image /> */}
       {/* <Practice /> */}
       {/* <TaskManager/> */}
       {/* <Form /> */}
-      <ProductList/>
     </div>
   );
 }

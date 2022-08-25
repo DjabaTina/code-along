1343cfba15bbcb138ef96097e5a8524f569bc3d1
DjabaTina <djabaaugustina1@gmail.com>
2022-08-25 15:24:33 +0100

    state-mangement-1

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,21 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import Product from "../components/Product";
 import Spinner from "../components/Spinner";
 import useAxios from "../hooks/useAxios";
+import { useProductContext } from "../components/context/productContext";
 
 function ProductList() {
+  // const { data, isLoading, error } = useAxios(
+  //   "https://api.escuelajs.co/api/v1/products"
+  // );
   const { data, isLoading, error } = useAxios(
-    "https://api.escuelajs.co/api/v1/products"
+    "https://fakestoreapi.com/products"
   );
-  //useEffect(()=> {
-  if (isLoading) return <Spinner />;
+  const { products, setProducts } = useProductContext();
 
-  if (error) return <p>{error}</p>;
-
+  useEffect(() => {
+    setProducts(data);
+  }, [data]);
+if (isLoading) {
+return <Spinner />;
+}
+if (error){
+  return <p>{error}</p>;}
+  console.log(products);
   return (
     <div className="flex flex-wrap gap-10 justify-center pt-10">
-      {data?.map((data) => (
+      {products?.map((data) => (
         <Product product={data} />
       ))}
     </div>
